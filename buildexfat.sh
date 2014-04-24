00dac84b1d8f25830728545f314f7fa2fbc32744
Santiago Angel <santiagoangel@gmail.com>
2014-04-24 14:19:21 -0500

    Create buildexfat.sh

---
+++ buildexfat.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+#cd /android/exfat-nofuse/
+make ARCH=arm KDIR=/android/source/kernel/ CROSS_COMPILE=/android/android-ndk-r9d/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+
